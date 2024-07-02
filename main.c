461d3bd7c4d1757ae139ce1f95431c453e4afe05
Atharv Parlikar <atharvparlikar@gmail.com>
2024-07-03 01:39:59 +0530

    compleated get_element function

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ typedef struct {
   float *data;
   int ndim;
   size_t *shape;
+  size_t *strides;
   size_t items;
   size_t itemsize;
 } ndarray;
@@ -17,11 +18,21 @@ ndarray Ndarray(size_t *shape, int ndim) {
   for (int i = 0; i < ndim; i++) {
     items *= shape[i];
   }
+
   x.items = items;
   x.data = (float *)malloc(items * sizeof(float));
   x.shape = shape;
   x.ndim = ndim;
-  x.itemsize = 32; // (4 byte | 32 bit) floats
+  x.itemsize = 4; // (4 byte | 32 bit) floats
+  x.strides = malloc((ndim - 1) * sizeof(float));
+  for (int i = 1; i < x.ndim; i++) {
+    int stride = 1;
+    for (int j = i; j < x.ndim; j++) {
+      stride *= x.shape[j];
+    }
+    x.strides[i - 1] = stride;
+  }
+
   return x;
 }
 
@@ -35,12 +46,14 @@ int get_linear_index(size_t *shape, int *indices, int ndim) {
   return linear_index;
 }
 
-float *get_element(ndarray *ndarr, int *indices, int indices_size) {
+ndarray get_element(ndarray *ndarr, int *indices, int indices_size) {
   if (ndarr->ndim == indices_size) {
     float element =
         ndarr->data[get_linear_index(ndarr->shape, indices, indices_size)];
-    float *returning_element = (float *)malloc(sizeof(float));
-    returning_element = (float[]){element};
+
+    ndarray returning_element = Ndarray((size_t[]){1}, 1);
+    returning_element.data[0] = element;
+
     return returning_element;
   } else {
     if (indices_size > ndarr->ndim) {
@@ -48,9 +61,41 @@ float *get_element(ndarray *ndarr, int *indices, int indices_size) {
       exit(1);
     } else { // because of previous conditions this just means
              // index_dimensions < ndim
-      //  TODO: Figure out array substringing without copying to allow in memory
+      //  TODO: Figure out array subscripting without copying to allow in memory
       //        array change.
-      return (float *)malloc(sizeof(float));
+
+      int x = ndarr->ndim - indices_size;
+      size_t size = 1;
+
+      for (int i = x; i < ndarr->ndim; i++) {
+        size *= ndarr->shape[i];
+      }
+
+      float *data = (float *)malloc(size * sizeof(float));
+      int start_index = 0;
+      int ndim = ndarr->ndim - indices_size;
+      size_t *shape = (size_t *)malloc(ndim * sizeof(size_t));
+
+      int count = 0;
+      for (int i = ndim; i < ndarr->ndim; i++) {
+        shape[count++] = ndarr->shape[i];
+      }
+
+      for (int i = 0; i < indices_size; i++) {
+        start_index += indices[i] * ndarr->strides[i];
+      }
+
+      printf("Start Index: %d\n", start_index);
+      count = 0;
+      for (int i = start_index;
+           i < start_index + ndarr->strides[indices_size - 1]; i++) {
+        data[count++] = ndarr->data[i];
+      }
+
+      ndarray subarr = Ndarray(shape, ndim);
+      subarr.data = data;
+
+      return subarr;
     }
   }
 }
@@ -66,16 +111,24 @@ void flatten(ndarray *ndarr) {
 }
 
 int main() {
-  ndarray x = Ndarray((size_t[]){3, 3, 2}, 3);
-  x.data[3] = 69;
+  ndarray x = Ndarray((size_t[]){3, 3, 3}, 3);
+
+  for (int i = 3; i < 6; i++) {
+    x.data[i] = 1.0f;
+  }
   for (int i = 0; i < x.items; i++) {
     printf("%f\n", x.data[i]);
   }
-  int indices[3] = {0, 1, 0};
-  float *element = get_element(&x, indices, 4);
-  printf("Retrieved Element: %f\n", element[0]);
-  set_element(&x, indices, 70);
-  element = get_element(&x, indices, 3);
-  printf("Changed Element: %f\n", element[0]);
+
+  printf("========================\n");
+
+  ndarray element = get_element(&x, (int[]){1}, 1);
+
+  for (int i = 0; i < element.ndim; i++) {
+    printf("%zu ", element.shape[i]);
+  }
+
+  printf("\n");
+
   return 0;
 }
